6c04cce7858ea8ca6f5c341d45230505d783c58c
Sladi <sladiri@users.noreply.github.com>
2017-05-08 00:16:55 +0200

    log signal starts and ends.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import { Container } from "cerebral/react";
 import controller from "./controller";
 import App from "./app";
 
+// When a function tree is executed
+controller.on("start", (execution, payload) => {
+  console.log("function tree start", { execution, payload });
+});
+
+// When a function tree execution has ended
+controller.on("end", (execution, payload) => {
+  console.log("function tree end", { execution, payload });
+});
+
 render(
   <Container controller={controller}>
     <App />
