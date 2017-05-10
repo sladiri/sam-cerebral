3fad33a0775cc63a8252e92a4d12be28836d38d5
Sladi <sladiri@users.noreply.github.com>
2017-05-10 23:12:40 +0200

    Add eslint rule.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const o = { a: 42 };
 const p = { b: 666 };
-const q = { ...o, ...p, c: 123 };
+({ ...o, ...p, c: 123 });
 
-async function* gfn(a, b, c) {
+async function* gfn() {
   const promises = [123, 42, 666];
   while (promises.length) {
     yield await Promise.resolve(promises[0]);
