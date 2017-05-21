7d47eb75f29e56c77bad67b8f4d88996b94f5056
Sladi <sladiri@users.noreply.github.com>
2017-05-21 13:45:04 +0200

    Add render on server example.

--- src/app/boundary/component.js
+++ src/app/boundary/component.js
@@ -2,6 +2,21 @@ import { connect } from "cerebral/react";
 import { state, signal } from "cerebral/tags";
 import { views } from "./view";
 
+export function computeAppViewModel(
+  { increase, decrease, cancel, count, ...connectedProps },
+  parentProps,
+) {
+  return {
+    ...parentProps,
+    ...connectedProps,
+    actions: { increase, decrease, cancel },
+    model: { count },
+  };
+}
+
+export const appView = ({ controlStateName, ...props }) =>
+  controlStateName ? views[controlStateName](props) : null;
+
 export const App = connect(
   {
     controlStateName: state`sam.controlState.name`,
@@ -11,17 +26,6 @@ export const App = connect(
     decrease: signal`decrease`,
     cancel: signal`cancel`,
   },
-  function computeAppViewModel(
-    { increase, decrease, cancel, count, ...connectedProps },
-    parentProps,
-  ) {
-    return {
-      ...parentProps,
-      ...connectedProps,
-      actions: { increase, decrease, cancel },
-      model: { count },
-    };
-  },
-  ({ controlStateName, ...props }) =>
-    controlStateName ? views[controlStateName](props) : null,
+  computeAppViewModel,
+  appView,
 );
