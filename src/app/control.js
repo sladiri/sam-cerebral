8127f51c752f8dd191a4347e0e4697a40c1dd854
Sladi <sladiri@users.noreply.github.com>
2017-05-21 13:22:00 +0200

    Change control state example.

--- src/app/control.js
+++ src/app/control.js
@@ -16,9 +16,9 @@ export async function cancel() {
 
 export function computeControlState(model) {
   if (Number.isInteger(model.count)) {
-    if (model.count <= -2) return ["small", ["increase", "cancel"]];
+    if (model.count < -5) return ["small", ["increase", "cancel"]];
 
-    if (model.count >= 2) return ["big", ["decrease", "cancel"]];
+    if (model.count > 5) return ["big", ["decrease", "cancel"]];
 
     return ["normal", ["increase", "decrease", "cancel"]];
   }
