d0e50966c4fcf41f0fd8c0482f7e544204fb4019
Sladi <sladiri@users.noreply.github.com>
2017-06-16 09:52:34 +0200

    Fix styles server render flashing.

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,27 @@
 import "babel-polyfill";
 import "setimmediate";
 import h from "react-hyperscript";
+import serve from "koa-static";
+import Koa from "koa";
+import * as ReactFreeStyle from "react-free-style";
 import { UniversalController } from "cerebral";
 import { Container } from "cerebral/react";
 import { renderToString } from "react-dom/server";
 import { module, view } from "../universal/app/boundary";
-import serve from "koa-static";
-import Koa from "koa";
-import * as ReactFreeStyle from "react-free-style";
 
 const app = new Koa();
 
 app.use(serve("./static"));
 
-app.use(async ctx => {
+app.use(ctx => {
   const controller = UniversalController(module);
   controller.getSignal("init")({});
   controller.getSignal("napSack.init")({});
 
-  const styles = ReactFreeStyle.rewind().toString();
   const html = renderToString(h(Container, { controller }, h(view)));
   const script = controller.getScript();
+
+  const styles = ReactFreeStyle.rewind().toString(); // Run last to prevent empty styles.
 
   ctx.body = `
     <!doctype html>
