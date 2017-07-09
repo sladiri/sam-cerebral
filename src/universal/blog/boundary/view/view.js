62c174f0c0b22a4e8b814aec076501d306c9f82e
Sladi <sladiri@users.noreply.github.com>
2017-07-09 22:54:05 +0200

    add hint in view

--- src/universal/blog/boundary/view/view.js
+++ src/universal/blog/boundary/view/view.js
@@ -64,7 +64,7 @@ export default function Blog({
       <SamStateIndicator styles={styles} />
 
       <p>
-        User: {model.userName || "none"}
+        User: {model.userName || "none (log in to post)"}
       </p>
 
       {userPanel}
