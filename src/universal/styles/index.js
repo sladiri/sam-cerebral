f97d36466d6e9e54de4e28fd77bd24b269c8be7b
Sladi <sladiri@users.noreply.github.com>
2017-07-20 10:51:42 +0200

    refac(parseTachyons.css): move to own file.

--- src/universal/styles/index.js
+++ src/universal/styles/index.js
@@ -2,55 +2,7 @@ import { styled } from "react-free-style";
 import Color from "color";
 import { pick, curry } from "ramda";
 
-import tachyonsParsedJson from "./tachyonsParsed.json";
-
-const splitRulePerSelector = ({ selectors, declarations }, checkNonClasses) => {
-  const rules = {};
-  for (const selector of selectors) {
-    if (!selector.startsWith(".")) {
-      if (checkNonClasses)
-        throw new Error(
-          "Parser not implemented for media query",
-          selectors,
-          selector,
-        );
-      continue;
-    }
-
-    const key = selector.substr(1);
-    rules[key] = declarations;
-  }
-  return rules;
-};
-
-const splitMediaqueryPerRule = val => {
-  if (val.value.length !== 1) throw new Error("unexpected media value", val);
-
-  const rules = {};
-  const mediaKey = `${val.type} ${val.value[0]}`;
-  const mediaRules = val.nestedRules.map(splitRulePerSelector, true);
-  for (const nestedRules of mediaRules) {
-    for (const [key, value] of Object.entries(nestedRules)) {
-      rules[key] = {
-        [mediaKey]: value,
-      };
-    }
-  }
-  return rules;
-};
-
-const parseJson = (acc, val) => {
-  if (val.type === "rule") {
-    Object.assign(acc, splitRulePerSelector(val));
-  } else if (val.type === "@media") {
-    Object.assign(acc, splitMediaqueryPerRule(val));
-  } else {
-    throw new Error("Parsing not implemented for val", val);
-  }
-  return acc;
-};
-
-const tachyonsStyles = tachyonsParsedJson.value.reduce(parseJson, {});
+import tachyonsStyles from "./parseJson";
 
 export const colours = {
   warn: Color("brown").lighten(0.3).string(),
