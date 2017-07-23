e3b5fa8ee26465ee43c2680b5d699cbeb0ef68c2
Sladi <sladiri@users.noreply.github.com>
2017-07-23 10:14:36 +0200

    refac(blog.computed): use different compute API.

--- src/blog/boundary/computed.js
+++ src/blog/boundary/computed.js
@@ -1,16 +1,20 @@
 import { compute } from "cerebral";
 import { state } from "cerebral/tags";
 
-export const model = compute(get => ({
-  userName: get(state`blog.userName`),
-  posts: (get(state`blog.posts`) || []).map(({ _id, message, deleted }) => {
-    const [created, creator] = _id.split("-");
-    return {
-      id: _id,
-      creator,
-      created: new Date(Number.parseInt(created)).toLocaleString(),
-      message,
-      deleted,
-    };
+export const model = compute(
+  state`blog.userName`,
+  state`blog.posts`,
+  (userName, posts = []) => ({
+    userName,
+    posts: posts.map(({ _id, message, deleted }) => {
+      const [created, creator] = _id.split("-");
+      return {
+        id: _id,
+        creator,
+        created: new Date(Number.parseInt(created)).toLocaleString(),
+        message,
+        deleted,
+      };
+    }),
   }),
-}));
+);
