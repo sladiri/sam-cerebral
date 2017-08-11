968a9e8e28501b7bfe30af26ab6a1da716d6c0b0
Sladi <sladiri@users.noreply.github.com>
2017-08-11 16:44:22 +0200

    feat(app): optionally store cerebral debugger port in localstorage.

--- src/browserEntryPoint/boundary.js
+++ src/browserEntryPoint/boundary.js
@@ -7,10 +7,12 @@ import { Container } from "cerebral/react";
 import { moduleFactory } from "../appShell/control";
 import { view } from "../appShell/boundary";
 
+const debuggerPort = localStorage.getItem("cerebraldebuggerport") || 8585;
+
 const controller = Controller({
   ...moduleFactory({ hasServerState: !!window.CEREBRAL_STATE }),
   devtools: Devtools({
-    host: "localhost:8585",
+    host: `localhost:${debuggerPort}`,
     reconnect: false,
     preventExternalMutations: false, // prevent bug in current Cerebral
   }),
